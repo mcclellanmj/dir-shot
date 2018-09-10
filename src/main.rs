1ee36731a454f23dfc825196fc94d77ab7c59f2b
Matt McClellan <matt.mcclellan@altec.com>
2018-09-10 16:57:08 -0500

    Added multi-thread

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
 extern crate clap;
+extern crate walkdir;
+extern crate crossbeam_channel;
+extern crate num_cpus;
+
 use clap::{Arg, App};
+use walkdir::WalkDir;
+use crossbeam_channel as channel;
+use std::thread;
+use walkdir::DirEntry;
+use std::thread::JoinHandle;
+use std::fs::File;
+use std::io::Read;
+use std::str;
+use std::time::SystemTime;
 
 fn main() {
     let matches = App::new("Directory Capture")
@@ -11,5 +24,33 @@ fn main() {
             .index(1))
         .get_matches();
 
-    println!("Hello, world! {}", matches.value_of("directory").unwrap());
+    let (file_sender, file_receiver): (channel::Sender<DirEntry>, channel::Receiver<DirEntry>) = channel::bounded(10000);
+
+    let threads = (0..num_cpus::get() - 1).map(move |x| {
+        let local_receiver = file_receiver.clone();
+
+        let thread_builder = thread::Builder::new()
+            .name(format!("Thread {}", x))
+            .spawn(move || {
+                loop {
+                    if let Some(entry) = local_receiver.recv() {
+                        let metadata = entry.metadata().unwrap();
+
+                        println!("{}: [{}] - Modified: {:?}, Size: {}", thread::current().name().unwrap(),
+                                 entry.path().display(),
+                                 metadata.modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+                                 metadata.len());
+                    } else {
+                        break;
+                    }
+                }
+            });
+
+        return thread_builder.unwrap();
+    }).collect::<Vec<JoinHandle<()>>>();
+
+    for entry in WalkDir::new(matches.value_of("directory").unwrap()) {
+        let entry = entry.unwrap();
+        file_sender.send(entry);
+    }
 }
