f9d7d5f6e573c77de7563ed90cfcc5837b0b4403
Matt McClellan <mcclellan.mj@gmail.com>
2018-09-14 20:21:18 -0500

    Working on part of code to capture the directory structure

--- src/main.rs
+++ src/main.rs
@@ -1,72 +1,63 @@
 extern crate clap;
 extern crate walkdir;
-extern crate crossbeam_channel;
-extern crate num_cpus;
+extern crate csv;
 
-use clap::{Arg, App};
+use clap::{Arg, App, SubCommand, ArgMatches};
 use walkdir::WalkDir;
-use crossbeam_channel as channel;
-use std::thread;
 use walkdir::DirEntry;
-use std::thread::JoinHandle;
 use std::fs::File;
-use std::io::Read;
+use std::path::PathBuf;
+use std::io::{Read, stdout};
 use std::str;
+use csv::Writer;
 use std::time::SystemTime;
+
+struct FileSnap {
+    path: PathBuf,
+    modified: u64,
+    size: u64
+}
+
+fn as_row(file_snap: &FileSnap) -> Vec<String> {
+    vec!(file_snap.path.display().to_string(),
+         file_snap.modified.to_string(),
+         file_snap.size.to_string()
+    )
+}
+
+fn run_record(args: &ArgMatches) {
+    let mut csv_writer = csv::Writer::from_writer(stdout());
+    csv_writer.write_record(&["Path", "Modified", "Size"]);
+
+    for entry in WalkDir::new(args.value_of("directory").unwrap()) {
+        let entry = entry.unwrap();
+        let metadata = entry.metadata().unwrap();
+
+        let file_snap = FileSnap {
+            path: entry.path().to_owned(),
+            modified: metadata.modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
+            size: metadata.len()
+        };
+
+        csv_writer.write_record(as_row(&file_snap)).expect("An error occurred while writing the row");
+    }
+}
 
 fn main() {
     let matches = App::new("Directory Capture")
         .version("0.0.1")
         .author("Matt McClellan <mcclellan.mj@gmail.com>")
-        .arg(Arg::with_name("directory")
-            .help("Directory to capture")
-            .required(true)
-            .index(1))
-        .arg( Arg::with_name("threads")
-            .help("Number of threads to use")
-            .number_of_values(1)
-            .short("t"))
+        .subcommand(SubCommand::with_name("record")
+            .arg(Arg::with_name("directory")
+                .help("Directory to capture")
+                .required(true)
+                .index(1))
+            .about("Record directory capture to stdout"))
         .get_matches();
 
-    let (file_sender, file_receiver): (channel::Sender<DirEntry>, channel::Receiver<DirEntry>) = channel::bounded(10000);
-
-    let num_threads = matches
-        .value_of("threads")
-        .map(|x| x.parse::<usize>())
-        .unwrap_or(Ok(num_cpus::get()))
-        .unwrap();
-
-    let threads = (0..num_threads).map(move |x| {
-        let local_receiver = file_receiver.clone();
-
-        let thread_builder = thread::Builder::new()
-            .name(format!("Thread {}", x))
-            .spawn(move || {
-                loop {
-                    if let Some(entry) = local_receiver.recv() {
-                        let metadata = entry.metadata().unwrap();
-
-                        println!("{}: [{}] - Modified: {:?}, Size: {}", thread::current().name().unwrap(),
-                                 entry.path().display(),
-                                 metadata.modified().unwrap().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_secs(),
-                                 metadata.len());
-                    } else {
-                        break;
-                    }
-                }
-            });
-
-        return thread_builder.unwrap();
-    }).collect::<Vec<JoinHandle<()>>>();
-
-    for entry in WalkDir::new(matches.value_of("directory").unwrap()) {
-        let entry = entry.unwrap();
-        file_sender.send(entry);
+    match matches.subcommand() {
+        ("record", Some(m)) => run_record(m),
+        _ => println!("Unknown command")
     }
 
-    drop(file_sender);
-
-    for thread in threads {
-        thread.join().unwrap();
-    }
 }
